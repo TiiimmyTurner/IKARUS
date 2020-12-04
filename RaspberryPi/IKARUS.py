02ecac1f2effcd798ab29e55ee115f8c3be65e50
TiiimmyTurner <TiiimmyTurner@users.noreply.github.com>
2020-12-05 00:14:06 +0100

    added gps

--- RaspberryPi/IKARUS.py
+++ RaspberryPi/IKARUS.py
@@ -4,16 +4,29 @@ import RPi.GPIO as GPIO
 import adafruit_mpu6050, adafruit_bme280
 import Adafruit_BMP.BMP085 as BMP085
 import paho.mqtt.publish as publish
+import adafruit_gps
+import serial
 
 #setup
+
+#GPS setup
+# RX = board.RX
+# TX = board.TX
+# uart = busio.UART(TX, RX, baudrate=9600, timeout=30)
+uart = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=3000)
+gps = adafruit_gps.GPS(uart, debug=False)
+
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK220,1000')
+last_print = time.monotonic()
 
 #Sensor setup
 i2c = busio.I2C(board.SCL, board.SDA)
 
 bmp180 = BMP085.BMP085() #hier read_temperature() und read_pressure(), bei allen anderen sensor.temperature, etc.
-bmp280= adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
+bmp280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
 #bmp280_outside = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x75)
-mpu6050 = adafruit_mpu6050.MPU6050(i2c)
+#mpu6050 = adafruit_mpu6050.MPU6050(i2c)
 
 
 #Mosquitto setup
@@ -76,20 +89,31 @@ def filter(gyr, acc, dt):
         for x in range(2):
             angle[x] = gyrData[x]
 
+
+# clock
+
 start = 0
 end = 0
 measure = time.time()
 sended = time.time()
 while True:
+
     try:
         filter(mpu6050.gyro, mpu6050.acceleration, end - start)
     except:
         pass
-    start = time.time()
 
+    start = time.time()
     if time.time() - measure >= minMeasureDelay:
         measure = time.time()
 
+        gps.update()
+        if not gps.has_fix:
+            print('Waiting for fix...')
+        else:
+            dataset["gps_x"] = gps.latitude
+            dataset["gps_y"] = gps.longitude
+        
         dataset["rotation_x"] = -angle[0]
         dataset["rotation_y"] = angle[1]
         try:
